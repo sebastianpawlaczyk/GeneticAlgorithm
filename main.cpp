9af8af6528ef2c091b016ef48fa661c5c1397e56
Sebastian Pawlaczyk <spawlaczyk97@gmail.com>
2019-03-17 16:13:09 +0100

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "src/Individual.hpp"
+#include "src/struct/Set.hpp"
+#include "src/Generator.hpp"
+
+#include "Agent.hpp"
+
+int main() {
+
+	Agent agent;
+	agent.startProcessing();
+
+	return 0;
+}
